5a41c56d1806382de093273749a19c8b0d2f6916
Umair Muhammad <umairijaz2772@gmail.com>
2024-08-08 21:51:02 +0500

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home } from './Home';
 import Game from './components/supertictactoe/Game';
 
-const App = () =>{
-  return(
-    <Game />
-
-)};
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/supertictactoe" element={<Game />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
