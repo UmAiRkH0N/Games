91d2558569c026851d7ac2b672b1b3615e87c09b
Umair Muhammad <umairijaz2772@gmail.com>
2024-08-08 22:18:10 +0500

    add some changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,19 +5,19 @@ export const Home = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
       <h1 className="text-4xl font-bold mb-8">Games By Umair</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-4xl">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-4xl ">
       <Link to="/supertictactoe">
-        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg">
+        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg cursor-pointer">
           Super TicTacToe
         </div>
       </Link>
-        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg">
+        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg cursor-pointer">
           <span>Coming Soon</span>
         </div>
-        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg">
+        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg cursor-pointer">
           <span>Coming Soon</span>
         </div>
-        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg">
+        <div className="flex items-center justify-center h-40 bg-gray-800 rounded-lg cursor-pointer">
           <span>Coming Soon</span>
         </div>
       </div>
